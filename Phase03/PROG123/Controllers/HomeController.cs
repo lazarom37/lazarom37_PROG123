f87b6871222dd16a8be126239512aa8250cb850e
kingishksh <lazaromarcuslazaro@gmail.com>
2022-05-21 22:20:56 -0700

    done with phase 06

---
+++ Phase03/PROG123/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PROG123.DAL;
+using PROG123.Models;
+using PROG123.utils;
+
+namespace PROG123.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // this is for testing purpuse only.
+            DatabaseHelper dbh = new DatabaseHelper(_configuration);
+            ConnStatusModel status = dbh.GetConnectionStringAndConnectionStatus();
+            ViewBag.ConnStr = status.ConnStr;
+            ViewBag.DBStatus = status.DBConnectionStatus;
+            ViewBag.Exception = status.Exception;
+            return View();
+        }
+
+        // add your actions here 
+
+        public IActionResult Page2(PersonModel pm)
+        {
+            //Creating a viewbag
+            ViewBag.Something = "Created by Marcus Lazaro (Phase02)";
+
+            DALPerson dp = new DALPerson(_configuration);
+            string uID = dp.AddPerson(pm);
+            pm.PersonID = uID;
+            HttpContext.Session.SetString("uID", uID.ToString()); //Write to the session
+            string strUID = HttpContext.Session.GetString("uID"); //Reads from the session
+
+            //PersonModel
+            return View(pm);
+        }
+
+        public IActionResult Edit(PersonModel pm)
+        {
+            string uID = HttpContext.Session.GetString("uID");
+            //instantiate a DALPerson class
+            DALPerson dp = new DALPerson(_configuration);
+            PersonModel person = dp.getPerson(uID);
+
+            return View(person);
+        }
+    }
+}
---
+++ Phase06/PROG123/Controllers/LazaroController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using PROG123.DAL;
+using PROG123.Controllers;
+using PROG123.Models;
+
+namespace PROG123.Controllers
+{
+    public class LazaroController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public LazaroController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult LogIn(LogInCredentialsModel logInCredentialsModel)
+        {
+            DALPerson dp = new DALPerson(_configuration);
+            PersonModel person = dp.CheckLogInCredentials(logInCredentialsModel);
+            if (person == null)
+            {
+                ViewBag.LoginMessage = "login failed :(";
+            }
+            else
+            {
+                ViewBag.UserFirstName = person.FName;
+                ViewBag.LoginMessage = null;
+            }
+            return View("Index");
+        }
+    }
+}
